89c34caf26065234a2366a0c9c078cf86dc8629a
Dheer Jain <59198770+calcifer-3118@users.noreply.github.com>
2026-04-10 03:04:10 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+
+        vector<int> arr(26);
+
+        for(int i=0; i<s.length(); i++)
+        {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
+        }    
+
+        for(auto i:arr)
+            if(i != 0)
+                return false;
+        
+        return true;
+    }
+};
