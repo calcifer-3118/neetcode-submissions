0d2dccd902293cb38b93b9634a02cd649b2ad8b5
Dheer Jain <59198770+calcifer-3118@users.noreply.github.com>
2026-04-17 00:49:36 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string newStr = "";
+        for(auto &c:s)
+        {
+            if(isalnum(c))
+            {
+                newStr += tolower(c);
+            }
+        }
+
+        string rev = string(newStr.rbegin(), newStr.rend());
+        return newStr == rev;
+    }
+};
